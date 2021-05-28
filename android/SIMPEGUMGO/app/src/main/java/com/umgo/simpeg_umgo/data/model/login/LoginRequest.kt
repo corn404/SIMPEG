4edea4d3c033ec5859f736c6f114f104dc51ab8f
corn404 <codecorn404@gmail.com>
2021-05-28 16:12:03 +0800

    update

--- android/SIMPEGUMGO/app/src/main/java/com/umgo/simpeg_umgo/data/model/login/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.umgo.simpeg_umgo.data.model.login
-
-data class LoginRequest(
-    val username: String,
-    val password: String
-)
--- android/SIMPEGUMGO/app/src/main/java/com/umgo/simpeg_umgo/data/model/login/UpdatePasswordRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.umgo.simpeg_umgo.data.model.login
-
-data class UpdatePasswordRequest(
-    val id_pegawai: Int,
-    val password: String
-)
